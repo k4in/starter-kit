088c7f102bad04f2474bd2cd139350fdd6fbabf8
k4in <>
2024-01-31 17:49:51 +0100

    audit fix, small adjustments

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,3 +1,8 @@
 export default function Routes() {
-  return <div>Routes</div>;
+  return (
+    <div className="h-screen text-base layout">
+      <header className="bg-neutral-900">Header</header>
+      <main className="h-full bg-neutral-800">Content</main>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,25 @@ export default {
         screen: '100dvh',
       },
     },
+    screens: {
+      //breakpoints
+      sm: '37.5em',
+      md: '56.25em',
+      lg: '75em',
+      xl: '112.5em',
+    },
+    fontSize: {
+      xs: ['1.12rem', '1.456rem'],
+      sm: ['1.36rem', '1.768rem'],
+      base: ['1.6rem', '2.08rem'],
+      md: ['1.6rem', '2.08rem'],
+      lg: ['1.84rem', '2.392rem'],
+      xl: ['2.16rem', '2.808rem'],
+      '2xl': ['2.48rem', '1'],
+      '3xl': ['2.8rem', '1'],
+      '4xl': ['3.2rem', '1'],
+      '5xl': ['3.68rem', '1'],
+    },
   },
   plugins: [],
 };
